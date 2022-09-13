16de2a205cb4ac342427e8fa7279697b1ae7344d
zili <neodejack@gmail.com>
2022-09-14 01:11:49 +0800

    CJB-15 add inlinekeyboard selection and callback

--- hex/ports/app_driving.go
+++ hex/ports/app_driving.go
@@ -3,3 +3,9 @@ package ports
 type APIPorts interface {
 	EchoWords(words string) (string, error)
 }
+
+type AllocationViewerPorts interface {
+	GetDateSelection(dateStr string) (string, error)
+	CheckAllocationData(date string) (string, error)
+	SendAllocationDate(data string) error
+}
