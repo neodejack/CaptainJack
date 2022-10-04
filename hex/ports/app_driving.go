a4643a08a8982c589c4a4d5933184a26c094b10c
neodejack <75655215+neodejack@users.noreply.github.com>
2022-10-04 12:23:14 +0800

    clone production to beta (#5)
    * Cjb 15 create telebot UI that have inline keyboard selection (#2)
    
    * add gcp and os two adapters for secrets fetching
    
    gcp secrets manager is currently still not working. Currently main file is using the OS env var reading adapter to get secrets. will change this in the future
    
    * oops expose the keys
    
    * fix unit tests for secrets
    
    * add local testing bash script and put it in gitignore
    
    * adding github secrets to fix unit tests
    
    * CJB-15 add inlinekeyboard selection and callback
    
    * Update README.md
    
    * Update README.md
    
    * Deployment (#4)
    
    * add gcp and os two adapters for secrets fetching
    
    gcp secrets manager is currently still not working. Currently main file is using the OS env var reading adapter to get secrets. will change this in the future
    
    * oops expose the keys
    
    * fix unit tests for secrets
    
    * add local testing bash script and put it in gitignore
    
    * adding github secrets to fix unit tests

--- hex/ports/app_driving.go
+++ hex/ports/app_driving.go
@@ -3,3 +3,10 @@ package ports
 type APIPorts interface {
 	EchoWords(words string) (string, error)
 }
+
+type AllocationViewerPorts interface {
+	GetDateSelection(dateStr string) (string, error)
+	CheckAllocationData(date string) (string, error)
+	SendAllocationDate(data string) error
+}
+
--- hex/ports/core.go
+++ hex/ports/core.go
@@ -3,3 +3,7 @@ package ports
 type EchoingPort interface {
 	Echoing(words string) (string, error)
 }
+
+type AllocationViewerPort interface {
+	ViewAllocation(date string) (string, error)
+}
